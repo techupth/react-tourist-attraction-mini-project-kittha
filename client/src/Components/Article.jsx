039ecd1536d6075bbf00ffcf44eccd5d165df658
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-06-12 18:05:56 +0700

    fix: add rounded corner to picture

--- client/src/Components/Article.jsx
+++ client/src/Components/Article.jsx
@@ -32,7 +32,7 @@ function Article() {
       <p className="p-4">{article.description}</p>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:place-items-center gap-4 p-4">
         {article.photos.map((photo, index) => (
-          <img className="w-[400px]" key={index} src={photo} />
+          <img className="w-[400px] rounded-3xl" key={index} src={photo} />
         ))}
       </div>
       <div className="flex justify-center">
